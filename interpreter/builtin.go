2414bd0fbb475d1dc8e210ef2e27060baa579158
sukovanej <Milansuk@email.cz>
2018-11-11 19:11:27 +0100

    move string representation to

--- interpreter/builtin.go
+++ interpreter/builtin.go
@@ -3,7 +3,6 @@ package interpreter
 import (
     "fmt"
     "errors"
-    "strconv"
 )
 
 func createStringObject(value string) (*Object) {
@@ -20,53 +19,6 @@ func (object *Object) GetMetaObject() (*Object, error) {
         return MetaObject, nil
     }
     return object.Meta, nil
-}
-
-func (obj *Object) GetStringRepresentation(scope *Scope) (string, error) {
-    var err error
-    var value string
-
-    if stringObject, ok := obj.Slots["__string__"]; ok {
-        if stringObject.Type == TYPE_CALLABLE {
-            stringObject, err = stringObject.Slots["__call__"].Value.(ObjectCallable)([](*Object){ obj }, scope)
-            if err != nil { return "", err }
-
-            value, err = stringObject.GetString()
-        } else if stringObject.Type == TYPE_STRING {
-            value, err = stringObject.GetString()
-            if err != nil { return "", err }
-        } else {
-            return "", errors.New("Runtime error: __string__ must be of type string or callable.")
-        }
-    } else if obj.Type == TYPE_NUMBER {
-        number, err := obj.GetNumber()
-        if err != nil { return "", err }
-        value = strconv.FormatInt(number, 10)
-    } else if obj.Type == TYPE_FLOAT {
-        number, err := obj.GetFloat()
-        if err != nil { return "", err }
-        value = strconv.FormatFloat(number, 'E', -1, 10)
-    } else if obj.Type == TYPE_CALLABLE {
-        value = fmt.Sprintf("<callable> @ %p", obj)
-    } else if obj.Type == TYPE_STRING {
-        value, err = obj.GetString()
-        if err != nil { return "", err }
-    } else if obj.Type == TYPE_OBJECT {
-        value = "<object"
-
-        for key, object := range obj.Slots {
-            repr, err := object.GetStringRepresentation(scope)
-            if err != nil { return "", err }
-
-            value += " " + key + "=" + repr
-        }
-
-        value += ">"
-    } else {
-        panic("stirng representation not set")
-    }
-
-    return value, nil
 }
 
 func NewObject(objectType ObjectType, value interface{}, meta *Object, slots map[string](*Object)) *Object {
